08b46d6651fc0dc334e289792b4faf8a505a761d
Natalia Goncear <natalia.goncear@amdaris.com>
2020-02-05 19:18:03 +0200

    second commit

---
+++ IntroToCharp/HelloCSharp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace IntroToCharp
+{
+    class HelloCSharp
+    {
+        static void Main()
+        {
+            /*String FirstName = "Goncear";
+            String LastName = "Natalia";
+            System.Console.Write("My name is " + LastName + " "+ FirstName);
+            System.Console.WriteLine("\n1\n" + "101\n" + "1001\n");*/
+
+            //DateTime localDate = DateTime.Now;
+            //System.Console.WriteLine("Data and Time for now is " + localDate);
+
+            //Double sqrOfNumb = Math.Sqrt(12345);
+            //System.Console.WriteLine("Sqrt of 12345 will be " + sqrOfNumb);
+
+
+
+            //System.Console.WriteLine("Please inset your age \n");
+            //string myAge = Console.ReadLine();
+            //int _age = Int32.Parse(myAge) + 10;
+            //DateTime _date = DateTime.Now;
+            //_date = _date.AddYears(10);
+
+            //System.Console.WriteLine("On " + _date.ToString("dd/MM/yyyy") + " I will be " + _age +  " years old");
+
+
+            int m = 0;
+
+            while (m <= 100)
+            {
+                if (m%2==0)
+                {
+                    Console.WriteLine(m);
+                }
+                else
+                {                   
+                    Console.WriteLine(-m); }
+                m++;
+            }
+
+        }
+    }
+}
